3a7d7b89708325c7602cc5ea7013588ec6116fdc
Arnold Fernandes <afernandes@factset.com>
2020-07-28 12:29:41 -0400

    testing

--- codegen/CustomPythonClientCodegen.java
+++ codegen/CustomPythonClientCodegen.java
@@ -15,16 +15,16 @@ public class CustomPythonClientCodegen extends PythonClientCodegen {
 
         File folder = new File("/local/templates/python");
         File[] files = folder.listFiles();
-        for (File file : files)
-        {
-            System.out.println(file.getName());
-            if(file.getName().endsWith("_doc.mustache")) {
-                String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf("_doc.mustache"))) + ".md";
-                supportingFiles.add(new SupportingFile(file.getName(), apiDocPath, outFilename));
-            }
-            else {
-                String outFilename = file.getName().substring(0, file.getName().indexOf(".mustache")) + ".py";
-                supportingFiles.add(new SupportingFile(file.getName(), apiPackageDir, outFilename));
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+                if (file.getName().endsWith("_doc.mustache")) {
+                    String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf("_doc.mustache"))) + ".md";
+                    supportingFiles.add(new SupportingFile(file.getName(), apiDocPath, outFilename));
+                } else {
+                    String outFilename = file.getName().substring(0, file.getName().indexOf(".mustache")) + ".py";
+                    supportingFiles.add(new SupportingFile(file.getName(), apiPackageDir, outFilename));
+                }
             }
         }
     }
