3aca11b52f15f87c7130ee594b9cefc368ccf82a
afernandes85 <58183178+afernandes85@users.noreply.github.com>
2020-07-28 17:24:58 -0400

    chore(ci): modifying travis ci script (#1)

--- codegen/CustomCSharpNetCoreClientCodegen.java
+++ codegen/CustomCSharpNetCoreClientCodegen.java
@@ -16,16 +16,16 @@ public class CustomCSharpNetCoreClientCodegen extends CSharpNetCoreClientCodegen
 
         File folder = new File("/local/templates/csharp-netcore");
         File[] files = folder.listFiles();
-        for (File file : files)
-        {
-            System.out.println(file.getName());
-            if(file.getName().endsWith("_doc.mustache")) {
-                String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf("_doc.mustache"))) + ".md";
-                supportingFiles.add(new SupportingFile(file.getName(), apiDocPath, outFilename));
-            }
-            else {
-                String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf(".mustache"))) + ".cs";
-                supportingFiles.add(new SupportingFile(file.getName(), apiPackageDir, outFilename));
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+                if (file.getName().endsWith("_doc.mustache")) {
+                    String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf("_doc.mustache"))) + ".md";
+                    supportingFiles.add(new SupportingFile(file.getName(), apiDocPath, outFilename));
+                } else {
+                    String outFilename = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, file.getName().substring(0, file.getName().indexOf(".mustache"))) + ".cs";
+                    supportingFiles.add(new SupportingFile(file.getName(), apiPackageDir, outFilename));
+                }
             }
         }
     }
