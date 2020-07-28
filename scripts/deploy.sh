d5f2746821e108e9cd43e822488e337cf354c83f
Arnold Fernandes <afernandes@factset.com>
2020-07-28 17:21:23 -0400

    testing

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker images
-docker tag openapi-generator-cli-custom $DOCKER_USERNAME/openapi-generator-cli-custom:4.2.2
-docker push $DOCKER_USERNAME/openapi-generator-cli-custom:4.2.2
